8553332450ce98c9f2b84850c0698ed00909def2
Paul Kippes <kippesp@gmail.com>
2025-08-07 23:55:47 -0700

    Check if file opened successfully

--- my_computer/11v2/src/lib/util/textfile_reader.cpp
+++ my_computer/11v2/src/lib/util/textfile_reader.cpp
@@ -2,6 +2,7 @@
 
 #include <cstdio>
 #include <sstream>
+#include <stdexcept>
 
 #if defined _MSC_VER
 #pragma warning(disable : 4996)
@@ -12,6 +13,11 @@ namespace jfcl {
 TextFileReader::TextFileReader(const char* filename)
 {
   FILE* fd = fopen(filename, "r");
+  if (!fd)
+  {
+    throw std::runtime_error(std::string("Failed to open file: ") + filename);
+  }
+
   std::stringstream ss;
 
   int ch;
